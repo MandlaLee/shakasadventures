6bbaaa05c7bc5aec6a2b36cb50036d5090fcdaaf
Mandla Lee <finance.uniresidentia@gmail.com>
2026-06-19 14:46:24 +0200

    Update save.js

--- js/save.js
+++ js/save.js
@@ -1,1 +1,27 @@
+/* =========================================
+   SAVE SYSTEM (FUTURE)
+========================================= */
 
+function saveGame() {
+
+    const data = {
+        player,
+        huts,
+        npcs,
+        gameTime
+    };
+
+    localStorage.setItem("shaka_save", JSON.stringify(data));
+}
+
+function loadGame() {
+
+    const data = JSON.parse(localStorage.getItem("shaka_save"));
+
+    if (!data) return;
+
+    player = data.player;
+    huts = data.huts;
+    npcs = data.npcs;
+    gameTime = data.gameTime;
+}
