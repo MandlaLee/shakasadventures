d6b899087edad902ef7ad6eceb0ca4f8ad33a2bc
Mandla Lee <finance.uniresidentia@gmail.com>
2026-06-19 14:46:16 +0200

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,1 +1,24 @@
+/* =========================================
+   TIME SYSTEM
+========================================= */
 
+let gameTime = {
+    hour: 6,
+    minute: 0
+};
+
+function initTime() {}
+
+function updateTime() {
+
+    gameTime.minute += 0.1;
+
+    if (gameTime.minute >= 60) {
+        gameTime.minute = 0;
+        gameTime.hour++;
+    }
+
+    if (gameTime.hour >= 24) {
+        gameTime.hour = 0;
+    }
+}
