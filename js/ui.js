6b6c408688cfd0409791cb7db9e96b6f4b4ff7d9
Mandla Lee <finance.uniresidentia@gmail.com>
2026-06-19 14:46:19 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,17 @@
+/* =========================================
+   UI SYSTEM
+========================================= */
 
+function drawUI() {
+
+    const ctx = Game.ctx;
+
+    ctx.fillStyle = "#000";
+    ctx.font = "16px Arial";
+
+    ctx.fillText(
+        `Time: ${Math.floor(gameTime.hour)}:${Math.floor(gameTime.minute)}`,
+        20,
+        30
+    );
+}
